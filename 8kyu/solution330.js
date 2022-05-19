0ecfc43c59aa06958e66f203c5ee8ec8141daab2
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-05-19 14:34:38 -0400

    Playing with cubes II

---
+++ 8kyu/solution330.js
@@ -0,0 +1,34 @@
+/*
+Playing with cubes II
+
+Hey Codewarrior!
+
+You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code two: One taking an integer and one handling no given arguments!
+
+Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+
+The constructor taking no arguments should assign 0 to Cube's Side property.
+
+Will the parameter always be a number?
+Will the return always be a number?
+
+let cube = new Cube(12);
+console.log(cube.getSide(), 12);
+cube.setSide(42);
+console.log(cube.getSide(), 42);
+*/
+
+// This Cube class needs help
+// Implement the constructor so that it can take an integer for the side or no args
+class Cube {
+    constructor(side){
+      this.setSide(side);
+    }
+    
+    getSide() {
+      return this.side; 
+    }
+    setSide(side = 0) {
+      this.side = Math.abs(side);
+    }
+  }
