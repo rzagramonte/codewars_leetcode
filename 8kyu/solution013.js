82982f9a752a70756b635fc5e304d7c39323349e
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-05-21 11:48:07 -0400

    refactor solution013

--- 8kyu/solution013.js
+++ 8kyu/solution013.js
@@ -1,4 +1,6 @@
 /*
+Sum of positive
+
 You get an array of numbers, return the sum of all of the positives ones.
 
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
@@ -6,8 +8,8 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0.
 
 Will the parameter always be an array?
-Will the elements in the array always be numbers?
-Will the return always be a number?
+Will the elements in the input array always be numbers?
+Will the return always be one number?
 
 console.log(positiveSum([1,2,3,4,5]),1);
 console.log(positiveSum([1,-2,3,4,5]),13);
@@ -16,7 +18,8 @@ console.log(positiveSum([-1,-2,-3,-4,-5]),0);
 console.log(positiveSum([-1,2,3,4,-5]),9);
 */
 
-//declare variable and assign to arrow function
-//filter elements that satisfy callback function of being equal to or greater than 0
-//add the remaining elements to one number
-const positiveSum = arr => arr.filter(e=>e>=0).reduce((a,c)=>a+c,0);
+//declare constant variable and assign to arrow function
+//set parameter
+//reduce the elements in the array to one number
+//is the current value greater than 0 ? then return it, else return 0
+const positiveSum = arr => arr.reduce((a,c)=>a+(c>0?c:0),0);
