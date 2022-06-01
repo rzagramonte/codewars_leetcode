3c891d92b1f2c94bf246347e14b6d233c1eb86a3
Roosiel Agramonte <rzagramonte@outlook.com>
2022-06-01 14:43:28 -0400

    codewar challenge for 6-01

---
+++ 8kyu - 2022-06-01/solution002.js
@@ -0,0 +1,28 @@
+// Multiply
+// This code does not execute properly. Try to figure out why.
+
+// P.R.E.P.
+// P: parameters - two numbers of any kind a and b
+// R: return - product of two numbers
+// E: examples - 
+// 1 * 1 = 1
+// 1 * 2 = 2
+// 1 * 3 = 3
+// 1 * 4 = 4
+// 1 * 5 = 5
+// 1 * 6 = 6
+// 1 * 7 = 7
+// 1 * 8 = 8
+// 1 * 9 = 9
+// 1 * 10 = 10
+// 1 * 11 = 11
+// 1 * 12 = 12
+//P: pseudo code
+// create function that passes in two numbers
+// the numbers are multiplied
+// the product is returned
+
+function multiply(a, b){ // create function that passes in two numbers
+    return a * b// the product of the two multiplied numbers is returned
+  }
+  
