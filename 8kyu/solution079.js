653107a7db0a45567b67a241771b3404a0c7e00b
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-06-08 20:01:07 -0400

    refactor solution079

--- 8kyu/solution079.js
+++ 8kyu/solution079.js
@@ -7,29 +7,20 @@ current father's age (years)
 current age of his son (years)
 Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
 
-P.R.E.P.
-parameters: two numbers
-return: one number
-examples:
-input => output
-(36,7) => 22
-(55,30) => 5
-(42,21) => 0
-(22,1) => 20
-(29,0) => 29
-pseudocode:
-declare function
-set parameters
-create if else statement
-if dadYearsOld is greater than sonYears times two then return dadYears - (sonYearsOld times two)
-else return (sonYearsOld times two) - dadYearsOld
+Will the parameters always be two numbers?
+Will the numbers always be positive numbers?
+Will the return always be a number?
+Will the returned number always be greater than or equal to 0?
+
+console.log(twiceAsOld(55,30) , 5);
+console.log(twiceAsOld(42,21) , 0);
+console.log(twiceAsOld(22,1) , 20);
+console.log(twiceAsOld(29,0) , 29);
+console.log(twiceAsOld(36,7) , 22);
 */
 
-function twiceAsOld(dadYearsOld, sonYearsOld) {
-    // your code here
-    if (dadYearsOld > (sonYearsOld * 2)){
-        return dadYearsOld - (sonYearsOld * 2);
-      }else{
-        return (sonYearsOld * 2) - dadYearsOld;
-      }; 
-  }
+//declare const variable and assign to arrow function
+//set parameters
+//is dadYearsOld greater than songYearsOld * 2 ? then return dadYearsOld - sonYearsOld * 2
+//else return songYearsOld * 2 - dadYearsOld
+const twiceAsOld = (dadYearsOld, sonYearsOld) => dadYearsOld > (sonYearsOld * 2) ? dadYearsOld - (sonYearsOld * 2) : (sonYearsOld * 2) - dadYearsOld;
