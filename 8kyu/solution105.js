2c738518782b4545422c802d5177220336a667e1
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-06-29 10:28:08 -0400

    codewars solution for 6/29

---
+++ 8kyu/solution105.js
@@ -0,0 +1,69 @@
+/*
+Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+The Task
+Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+The Database
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+Possible invalid inputs include:
+
+IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+IP_ADDRESS_NOT_FOUND - ip address not in the database
+IP_ADDRESS_REQUIRED - no ip address was supplied
+
+P.R.E.P.
+parameters: a string that represents the user's language
+return: a string representing 'Welcome' in the user's langauge or defaults to 'Welcome' in English
+examples:
+input => output
+('english') => 'Welcome'
+('dutch') => 'Welkom'
+('IP_ADDRESS_INVALID') => 'Welcome'
+pseudocode:
+declare function greet
+set parameter langauge
+declare variable languages and assign it to an object containing key:value pairs of language: 'Welcome in language'
+return languages[langauge] which will return the value the key in the object languages OR languages['english'] which will return the value in the key 'english' (remember keys are strings) in the object langauges
+using [] will return the value in the key in the object
+using . will return the key or property in the object
+*/
+
+function greet(language) {
+	let languages = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  };
+  return languages[language] || languages['english'];
+}
