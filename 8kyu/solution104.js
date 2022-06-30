b11f2d90ec0992fbcfe9151d78f26e50da9306cb
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-06-30 09:43:37 -0400

    codewars solution for 6/30

---
+++ 8kyu/solution104.js
@@ -0,0 +1,63 @@
+/*
+Switch it Up!
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+
+P.R.E.P.
+parameters: a number 0-9
+return: a string representing the input number in words
+examples:
+input => output
+(1) => "One"
+(3) => "Three"
+(5) => "Five"
+pseudocode:
+declare function
+set parameter
+create switch statement to return specific string depending on different cases
+switch variable => number
+cases => 0-9
+code to run if case match => return string then break
+no need for default clause
+*/
+
+function switchItUp(number){
+    //Write your own Code!
+    switch(number){
+        case 0:
+            return 'Zero';
+            break;
+        case 1:
+            return 'One';
+            break;
+        case 2:
+            return 'Two';
+            break;
+        case 3:
+            return 'Three';
+            break;
+        case 4:
+            return 'Four';
+            break;
+        case 5:
+            return 'Five';
+            break;
+        case 6:
+            return 'Six';
+            break;
+        case 7:
+            return 'Seven';
+            break;
+        case 8:
+            return 'Eight';
+            break;
+        case 9:
+            return 'Nine';
+            break;
+    };
+    }
