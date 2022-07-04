8b6e4077439d0cf9ac5d28904534d91da17c5472
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-07-04 14:16:49 -0400

    codewars solution for 7/4

---
+++ 8kyu/solution100.js
@@ -0,0 +1,23 @@
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+P.R.E.P.
+parameter: none
+return: 5, a numerical value
+example: 
+input => output
+=> 5
+pseudocode:
+declare function
+declare variable fives and assign it to 'fives'
+return fives appended to the length property using dot notation
+*/
+
+function unusualFive() {
+    let fives = 'fives';
+    return fives.length;
+  }
