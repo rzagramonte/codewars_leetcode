1e2ccb926e2a080c9e0bc97c509cea0f72d4b8f0
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-07-05 14:04:49 -0400

    codewars solution for 7/5

---
+++ 8kyu/solution099.js
@@ -0,0 +1,16 @@
+/*
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+
+P.R.E.P.
+parameters: a variable
+return: must return false on its own and with the logical operator ! but these two also must equal each other
+examples: [] == false; ![] == false; [] == ![]
+pseudocode:
+declare const variable a and assign it to an empty array
+*/
+
+const a = [];
