fa9bd75f388b2d336c7c7fd27055afbb56473775
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-07-14 12:07:42 -0400

    codewars solution for 7/14

---
+++ 8kyu/solution088.js
@@ -0,0 +1,28 @@
+/*
+Do you speak "English"?
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+
+P.R.E.P.
+parameter: a string
+return: boolean of true or false
+example:
+input => output
+"abcEnglishdef" => true 
+"abcnEglishsef" => false
+"eNglisH" => true
+pseudocode:
+declare function
+set parameter
+return sentence appended to toLowerCase() appended to includes() with its argument as 'english'
+
+*/
+
+function squareArea(A){
+    return sentence.toLowerCase().includes('english');
+}
