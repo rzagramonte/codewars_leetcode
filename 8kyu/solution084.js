0cfa0659534bdb143ae32124b598f6f12658700b
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-07-18 10:00:43 -0400

    codewars solution for 7/18

---
+++ 8kyu/solution084.js
@@ -0,0 +1,29 @@
+/*
+Convert a string to an array
+Write a function to split a string and convert it into an array of words.
+
+Examples (Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+P.R.E.P.
+parameter: string
+return: array
+example:
+(Input ==> Output):
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+pseudocode:
+declare function
+set parameter
+return string appended to split() with its argument as ' '
+*/
+
+function stringToArray(string){
+
+	// code code code
+    return string.split(' ');
+
+}
