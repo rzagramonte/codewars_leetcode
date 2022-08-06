6eee6a9819c21e876c4e8eb9a0c4fae0b999c3ff
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-08-06 08:13:03 -0400

    codewars solution for 8/6

---
+++ 8kyu/solution066.js
@@ -0,0 +1,22 @@
+/*
+You Can't Code Under Pressure #1
+Code as fast as you can! You need to double the integer and return it.
+
+P.R.E.P.
+parameter: a number, i
+return: a number doubled
+examples:
+input => output
+2 => 4
+1 => 2
+9 => 18
+pseudocode:
+declare function
+set parameter
+return i * 2
+*/
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
