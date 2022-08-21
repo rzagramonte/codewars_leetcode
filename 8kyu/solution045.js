ce301ddf9098b908e2fbb6f514210574e0cd0950
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-08-21 08:13:37 -0400

    codewars solution for 8/21

---
+++ 8kyu/solution045.js
@@ -0,0 +1,32 @@
+/*
+Century From Year
+Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+
+P.R.E.P.
+parameters: a number (the year)
+return: a number (the century that pertains to the given year)
+examples:
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+pseudocode:
+declare function
+set parameter
+return Math.ceil(year/100)
+*/
+
+function century(year) {
+    // Finish this :)
+    return Math.ceil(year/100);
+  }
