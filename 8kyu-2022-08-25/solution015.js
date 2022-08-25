4ecbf5c14aae4541ded305a781b62fd0d3f59187
Roosiel Agramonte <rzagramonte@outlook.com>
2022-08-25 17:55:55 -0400

    codewars solution for 8-25

---
+++ 8kyu-2022-08-25/solution015.js
@@ -0,0 +1,17 @@
+/*
+ Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+
+Style Points
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+P. - parameters: none
+R. - return: returns the string of 'hello, world!'
+E. - examples/edge cases: 'hello, world!'
+P. - pseudocode: create a function declaration or assign an anon function value to a variable called greet
+use arrow function
+use the return keyword and the string 'hello, world!'
+*/
+
+const greet = () => 'hello world!';
+
+
