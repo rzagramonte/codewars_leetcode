657950327233a4e71663ad7c6db7be9d09d8cf66
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-08-28 13:31:38 -0400

    codewar solution for 8/28

--- 8kyu/solution040.js
+++ 8kyu/solution040.js
@@ -1,3 +1,24 @@
 /*
+Calculate average
+Write a function which calculates the average of the numbers in a given list.
 
+Note: Empty arrays should return 0.
+P.R.E.P.
+parameter: an array of numbers
+return: the average of the numbers or 0 if the array is empty
+example:
+input => output
+[7,6,8,5,4,9] => 6.5
+[5,42,1] => 16
+[] => 0
+pseudocode:
+declare function
+set parameter
+set if else statment using ternary operator
+if empty array return 0 else return array.reduce()/array.length
 */
+
+function findAverage(array) {
+    // your code here
+    return array.length === 0 ? 0 : array.reduce((acc,c)=> acc + c, 0) / array.length;
+  }
