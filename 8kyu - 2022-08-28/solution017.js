74975df3c5519c59a0b0eff169fdd9d1714797da
Roosiel Agramonte <rzagramonte@outlook.com>
2022-08-29 11:32:33 -0400

    codewars solution for 8-28

---
+++ 8kyu - 2022-08-28/solution017.js
@@ -0,0 +1,50 @@
+/*
+Gravity Flip
+If you've completed this kata already and want a bigger challenge, here's the 3D version
+
+Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
+
+There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
+
++---+                                       +---+
+|   |                                       |   |
++---+                                       +---+
++---++---+     +---+              +---++---++---+
+|   ||   |     |   |   -->        |   ||   ||   |
++---++---+     +---+              +---++---++---+
++---++---++---++---+         +---++---++---++---+
+|   ||   ||   ||   |         |   ||   ||   ||   |
++---++---++---++---+         +---++---++---++---+
+
+
+Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+
+Examples (input -> output:
+* 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+* 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+
+P.R.E.P.
+parameters:
+an array of numbers, a
+a certain side of the box, which can be either 'L' or 'R', d
+
+return: an array of numbers listed from least to greatest if gravity is r
+an array of numbers listed from greatest to least if gravity is l
+
+examples/edge cases:
+input -> output:
+* 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+[5,8,7,3,2]->[2,3,5,7,8]
+
+* 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+[8,6,4,5,9,0,1]->[9,8,6,5,4,1,0]
+pseudocode:
+create function declaration or use anon function
+if d = l return min to max
+else return max to min
+*/
+
+const flip=(d, a)=>{
+    //TODO
+     return (d === 'R' ? a.sort((a,b)=>a-b) : a.sort((a,b)=>b-a));
+  }
