9fa3db4fe1f14f906da9c90c0422dd9ce300f0c9
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-09-04 13:13:27 -0400

    codewar solution for 9/4

---
+++ 8kyu - 2022-09-04/solution035.js
@@ -0,0 +1,34 @@
+/*
+Is the string uppercase?
+Task
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+P.R.E.P.
+parameters: a string
+return: boolean of true or false
+examples:
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+pseudocode:
+declare function
+set parameter
+if str === str.toUpperCase() return true else return false
+*/
+
+String.prototype.isUpperCase = function(str) {
+    // your code here
+    return this==this.toUpperCase();
+  }
