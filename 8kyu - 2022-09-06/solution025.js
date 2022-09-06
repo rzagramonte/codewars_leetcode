b922fec547643bddf4a6a57f4558363f5e4c158e
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-09-06 15:46:27 -0400

    codewar solution for 9/6

---
+++ 8kyu - 2022-09-06/solution025.js
@@ -0,0 +1,21 @@
+/*
+Returning Strings
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+P.R.E.P.
+parameters: name as the string containing a name
+return: "Hello, <name> how are you doing today?".
+examples: 
+input: Roosiel
+output: "Hello, Roosiel how are you doing today?".
+pseudocode:
+create a function
+create a parameter
+return string using template literal
+*/
+
+function greet(name){
+    //your code here
+    return `Hello, ${name} how are you doing today?`;
+  }
