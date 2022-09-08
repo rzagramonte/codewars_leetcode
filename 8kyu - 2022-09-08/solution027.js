e0aa9e41b6294aa7c88b798ddd19e792ae2142cb
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-09-08 09:01:59 -0400

    codewars solution for 9/8

---
+++ 8kyu - 2022-09-08/solution027.js
@@ -0,0 +1,19 @@
+/*
+MakeUpperCase
+Write a function which converts the input string to uppercase.
+P.R.E.P.
+parameters: str as a string
+return: return string in uppercase
+examples: 
+input: "hello" output: "HELLO"
+pseudocode:
+declare a function
+set parameter str
+use the method toUpperCase
+return the value of the method appended to the string
+*/
+
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase();
+  }
