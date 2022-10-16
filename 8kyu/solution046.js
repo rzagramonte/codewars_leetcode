c7bebdfbe84cb21f088ed2eff71693a686d418fe
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-10-15 23:48:25 -0400

    refactor solution046

--- 8kyu/solution046.js
+++ 8kyu/solution046.js
@@ -14,19 +14,19 @@ time = 6.7---> litres = 3
 
 time = 11.8--> litres = 5
 
-P.R.E.P.
-parameters: a number which represents the time in hours
-return: a number which represents the number of litres
-examples:
-time = 3 ----> litres = 1
-time = 6.7---> litres = 3
-time = 11.8--> litres = 5
-pseudocode:
-declare a function litres
-set a parameter time
-return Math.floor(time * .5)
+Will the parameter always be a number?
+Will the return always be a number?
+
+console.log(litres(2), 1);
+console.log(litres(1.4), 0);
+console.log(litres(12.3), 6);
+console.log(litres(0.82), 0);
+console.log(litres(11.8), 5);
+console.log(litres(1787), 893);
+console.log(litres(0), 0);
 */
 
-function litres(time) {
-    return Math.floor(time * .5);
-  }
+//declare variable and assign to arrow function
+//set parameter
+//return time times 1/5 and round down the answer
+const litres = time => Math.floor(time * .5);
