0fe05026ed7894e562100bd544a18e608ac4685b
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-10-18 21:13:28 -0400

    refactor solution066

--- 8kyu/solution066.js
+++ 8kyu/solution066.js
@@ -2,21 +2,9 @@
 You Can't Code Under Pressure #1
 Code as fast as you can! You need to double the integer and return it.
 
-P.R.E.P.
-parameter: a number, i
-return: a number doubled
-examples:
-input => output
-2 => 4
-1 => 2
-9 => 18
-pseudocode:
-declare function
-set parameter
-return i * 2
-*/
+Will the parameter always be a number?
+Will the return always be a number?
 
-function doubleInteger(i) {
-    // i will be an integer. Double it and return it.
-    return i * 2;
-  }
+console.log(doubleInteger(2), 4);
+*/
+const doubleInteger = i => i * 2;
