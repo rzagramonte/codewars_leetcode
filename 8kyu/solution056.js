cf7ff89dcb2a07a5f22c2afc11ee9708b36510f9
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-10-22 20:39:25 -0400

    refactor solution056

--- 8kyu/solution056.js
+++ 8kyu/solution056.js
@@ -4,23 +4,19 @@ Is he gonna survive?
 A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
 
 Return True if yes, False otherwise :)
-P.R.E.P.
-parameters: two non-negative integers
-return: a boolean of true or false
-example:
-input => output
-(3,10) => false
-(55,24) => true
-pseudocode:
-declare a function named hero
-set parameters bullets and dragons
-assign a variable bulletsNeeded to dragons * 2
-create if else statement using ternary operator
-if bullets >= bulletsNeeded return true else false
+
+Will the parameters always be two numbers?
+Will the return always be a boolean of true or false?
+
+console.log(hero(10, 5), true);
+console.log(hero(7, 4), false);
+console.log(hero(4, 5), false);
+console.log(hero(100, 40), true);
+console.log(hero(1500, 751), false);
+console.log(hero(0, 1), false);
 */
 
-function hero(bullets, dragons){
-    //Get Coding!
-      let bulletsNeeded = dragons * 2;
-      return bullets >= bulletsNeeded ? true : false;
-    }
+//declare variable and assign to arrow function
+//set parameters
+//return true or false if bullets are greater than or equal to dragons times 2
+const hero = (bullets, dragons) => bullets >= dragons * 2;
