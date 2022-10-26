5fb8da3af6262232ce3c08f804e604d1383c95f4
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-10-25 21:37:01 -0400

    refactor solution051

--- 8kyu/solution051.js
+++ 8kyu/solution051.js
@@ -7,21 +7,18 @@ invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 You can assume that all values are integers. Do not mutate the input array/list.
 
-P.R.E.P.
-parameters: an array of numbers
-return: the additive inverse of each; positive becomes negative and negative becomes positive
-examples:
-invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
-invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
-invert([]) == []
-pseudocode:
-declare function
-set parameter
-use ternary operator for if else statement
-if empty array return the array
-else return => use map method to muliply each element by -1
-*/
+Will the parameter be an array?
+Will the elements in the array be numbers?
+Can there be an empty array?
+Will the return always be an array?
 
-function invert(array) {
-    return array.length === 0 ? array : array.map(element=>element * -1);
-  }
+console.log(invert([1,2,3,4,5]), [-1,-2,-3,-4,-5]);
+console.log(invert([1,-2,3,-4,5]), [-1,2,-3,4,-5]);
+console.log(invert([]), []);
+console.log(invert([0]), [-0]);
+*/
+//declare variable and assign to arrow function
+//set parameter
+//if the length of the array is truthy return a new array where each element is negated
+//else return the input array
+const invert = array => array.length ? array.map(element=>-element) : array;
