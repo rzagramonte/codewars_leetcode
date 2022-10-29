cfef3a2b0d6c1be8232e0974410d362068f79edb
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-10-29 19:50:15 -0400

    refactor solution042

--- 8kyu/solution042.js
+++ 8kyu/solution042.js
@@ -9,41 +9,23 @@ If the input is an empty array or is null, return an empty array.
 Example
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
-P.R.E.P.
-parameters: an array of integers
-return: an array where input[0] count of positive numbers and where input[1] === sum of negative numbers; ignore 0
-examples:
-For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
-pseudocode:
-1. declare function
-2. set parameter
-3. declare variable emptyArr and variable newArr each to an empty array
-4. create if else statement
-5. if input is null 
-    a. return emptyArr
-6. else if input is an empty array 
-    a. return emptyArr
-7. else
-    a. declare variable posNums and set to input.filter(element => element > 0)
-    b. newArr.push(posNums.length)
-    c. declare variable negNums and set to input.filter(element => element < 0)
-    d. newArr.push(negNums.reduce((acc,c)=>acc+c,0))
-    e. return newArr
+Will the parameter always be an array, null, or an empty array?
+If the array has elements, will the elements of the input array be all numbers?
+Will the numbers be a mix of positive and negative numbers?
+Will the return always be an array?
+Will the first element of the returned array always be a positive number?
+Will the first element of the returned array represent the length of an array containing all the positive numbers of the input array?
+Will the second element of the returned array always be a negative number?
+Will the second element of the returned array always be a reduced value of an array containing all the negative numbers of the input array?
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]),[10, -65]);
+console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]),[8, -50]);
 */
 
-function countPositivesSumNegatives(input) {
-    // your code here
-    let emptyArr = [];
-    let newArr = [];
-    if (input === null){
-        return emptyArr;
-    }else if(input.length === 0){
-        return emptyArr;
-    }else{
-        let posNums = input.filter(element => element > 0);
-        newArr.push(posNums.length);
-        let negNums = input.filter(element => element < 0);
-        newArr.push(negNums.reduce((acc,c) => acc + c, 0));
-        return newArr;
-    };
-  }
+//declare variable and assign to arrow function
+//set parameter i for input
+//return empty array if negation of input is truthy OR if the negation of the length of input is truthy 
+//else return an array with the following:
+//first element in array: the length of the filtered input; callback function returns true if any element is greater than 0
+//second element in array: a reduced value of the filtered input; callback function returns true if any element is less than 0
+const countPositivesSumNegatives = i => !i || !i.length ? [] : [i.filter(e => e > 0).length,i.filter(e => e < 0).reduce((acc,c) => acc + c, 0)];
