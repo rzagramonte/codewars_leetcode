bf8375386975c0aa203fb4a4ff3d3c130b0397e0
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-04 18:27:49 -0400

    refactor solution040

--- 8kyu/solution040.js
+++ 8kyu/solution040.js
@@ -3,22 +3,17 @@ Calculate average
 Write a function which calculates the average of the numbers in a given list.
 
 Note: Empty arrays should return 0.
-P.R.E.P.
-parameter: an array of numbers
-return: the average of the numbers or 0 if the array is empty
-example:
-input => output
-[7,6,8,5,4,9] => 6.5
-[5,42,1] => 16
-[] => 0
-pseudocode:
-declare function
-set parameter
-set if else statment using ternary operator
-if empty array return 0 else return array.reduce()/array.length
+
+Will the parameter be an array?
+Will the return be a number?
+
+console.log(findAverage([1,1,1]), 1);
+console.log(findAverage([1,2,3]), 2);
+console.log(findAverage([1,2,3,4]), 2.5);
 */
 
-function findAverage(array) {
-    // your code here
-    return array.length === 0 ? 0 : array.reduce((acc,c)=> acc + c, 0) / array.length;
-  }
+//declare variable and assign to arrow function
+//set parameter
+//if the length of the array is truthy, return the array reduced to one number by adding all the elements and then dividing by the length of the array
+//else return 0
+const findAverage = array => array.length ? array.reduce((acc,c)=> acc + c, 0) / array.length : 0;
