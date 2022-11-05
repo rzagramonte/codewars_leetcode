4371858d1cc4be8eb4a5bcb66cc17769db571c19
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-05 00:13:42 -0400

    codewars solution for 11/4

---
+++ 8kyu/solution131.js
@@ -0,0 +1,34 @@
+/*
+pick a set of first elements
+
+Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+
+Will there always be two parameters?
+Will the parameter arr always be an array?
+Will the default for parameter n always be the number 1?
+Will n always be a positive integer?
+
+Will the return always be an array?
+
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+*/
+
+function first(arr, n = 1) {//set n default value to 1
+    //create if else statement using ternary operator
+    //if n == 0  return an empty array
+    //else append the slice method to arr and pass in 0 as the first argument and n as the second argument
+    return n == 0 ? [] : arr.slice(0,n);
+  }
