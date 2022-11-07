880e420d27cd0294bd8581ab9920424a7127abce
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-07 17:41:00 -0500

    refactor solution037

--- 8kyu/solution037.js
+++ 8kyu/solution037.js
@@ -3,19 +3,18 @@ Beginner - Reduce but Grow
 Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
-P.R.E.P.
-parameters: x a non-empty array of numbers
-return: the product of all the numbers
-examples:
-input => output
-[1,1,1,1,1] => 1
-[8,6,7,4,5,3] => 20160
-pseudocode:
-declare function
-set parameter x
-append x to the reduce function multiplying all the values to return the product
+
+Will the parameter always be an array?
+Will the return be a number?
+
+console.log(grow([1, 2, 3]), 6);
+console.log(grow([4, 1, 1, 1, 4]), 16);
+console.log(grow([2, 2, 2, 2, 2, 2]), 64);
 */
 
-function grow(x){
-    return x.reduce((acc, c) => acc * c, 1);
-  }
+//declare variable and assign to arrow function
+//set parameter
+//reduce array to one value
+//multiply all elements
+//initial value should be set to 1
+const grow = x => x.reduce((acc, c) => acc * c, 1);
