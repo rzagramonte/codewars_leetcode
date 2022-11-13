ab465155a23d60664a7f9f7d59818f2c7489bd60
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-12 23:38:36 -0500

    refactor solution032

--- 8kyu/solution032.js
+++ 8kyu/solution032.js
@@ -6,32 +6,33 @@ You can not use multiplier "*" operator.
 
 If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).
 
-P.R.E.P.
-parameters:
-name
-price = 30 unless otherwise stated
-return:
-a number
-examples:
-input => output
-Sydney Cooper-Smith, 78 => 1482
-Arnold Dane => 330
-Jennifer Kane, 15 => 195
-pseudocode:
-declare a anon arrow function
-set parameters name and price = 30
-set variable finalCost to 0
-set variable nameSplit to name and append the split method
-iterate over the array nameSplit
-for each character, add the variable price and reassign the variable finalCost to the new value
-return finalCost
+Will the parameters be a string and an optional number?
+Will the return always be a number?
+
+console.log(billboard("Jeong-Ho Aristotelis"), 600);
+console.log(billboard("Abishai Charalampos"), 570);
+console.log(billboard("Idwal Augustin"), 420);
+console.log(billboard("Hadufuns John",20), 260);
+console.log(billboard("Zoroaster Donnchadh"), 570);
+console.log(billboard("Claude Miljenko"), 450);
+console.log(billboard("Werner Vígi",15), 165);
+console.log(billboard("Anani Fridumar"), 420);
+console.log(billboard("Paolo Oli"), 270);
+console.log(billboard("Hjalmar Liupold",40), 600);
+console.log(billboard("Simon Eadwulf"), 390);
 */
 
+// declare a variable and assign it to an arrow function
+// set parameters name and price = 30
 const billboard = (name, price = 30) => {
+    // set variable finalCost to 0
     let finalCost = 0;
-    let nameSplit =  name.split('');
-    nameSplit.forEach(char =>{
+    //split string
+    //iterate over array
+    name.split('').forEach(char =>{
+        //add finalCost and price for each element
         finalCost += price;
     });
+    //return finalCost
     return finalCost;
 };
