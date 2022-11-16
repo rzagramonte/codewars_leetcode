3583c2264780184dcd8164fb3130d637b40c25c1
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-15 23:04:15 -0500

    refactor solution031

--- 8kyu/solution031.js
+++ 8kyu/solution031.js
@@ -12,20 +12,20 @@ Let's look at a few examples:
 array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
 array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 
-P.R.E.P.
-parameters: an array with positive numbers array and a non-negative number N
-return: N-th power of the element in the array with the index N. If N is outside of the array, then return -1
-example:
-array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
-array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
-pseudocode:
-declare function index
-set parameters array and n
-if n < array.length, return Math.pow(array[n],n)
-else return -1
+Will the parameters always be an array and a number?
+Will the elements in the array always be numbers
+Will the return always be a number?
+
+console.log(index([1, 2, 3, 4],2),9);
+console.log(index([1, 3, 10, 100],3),1000000);
+console.log(index([1, 2],3),-1);
+console.log(index([1,1,1,1,1,1,1,1,1,1], 9),1);
+console.log(index([1,1,1,1,1,1,1,1,1,2], 9),512);
+console.log(index([29,82,45,10], 3),1000);
+console.log(index([6,31], 3),-1);
+console.log(index([75,68,35,61,9,36,89,0,30], 10),-1);
 */
 
-function index(array, n){
-    //your code here
-    return array.length > n ? Math.pow(array[n], n) : -1;
-  };
+//declare variable and assign to arrow function
+//if the length of the array is greater than n, return element to the nth power, else return -1
+const index = (array, n) => array.length > n ? Math.pow(array[n], n) : -1;
