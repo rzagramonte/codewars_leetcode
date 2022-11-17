c0911eaf5369684d4a6127b08bce8eb3fc098d42
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-16 21:23:59 -0500

    refactor solution029/8kyu

--- 8kyu/solution029.js
+++ 8kyu/solution029.js
@@ -14,28 +14,21 @@ The correct answer would be 17.
 
 Hint: Don't forget to check for bad values like null/undefined
 
-P.R.E.P.
-parameters: an array
-return: a number representing the number of booleans as true there are in the array
-examples/edge cases:
-input => output
-[true,false,true] => 2
-pseudocode:
-declare function named countSheeps
-set parameter named arrayOfSheep
-set variable named result to the value of a new array
-use the map function to iterate over each element passed in through the parameter
-if element is truthy, push it to the new array
-return the length of the new array
+Will the parameter always be an array?
+Will the elements in the array always be either true or false?
+Will the return always be a number?
+
+var array1 = [true,  true,  true,  false,
+              true,  true,  true,  true ,
+              true,  false, true,  false,
+              true,  false, false, true ,
+              true,  true,  true,  true ,
+              false, false, true,  true ];
+console.log(countSheeps(array1), 17);
 */
 
-function countSheeps(arrayOfSheep) {
-    // TODO May the force be with you
-    let result = [];
-    arrayOfSheep.map(element => {
-      if(element){
-        result.push(element);
-      }
-    });
-    return result.length;
-  }
+//declare variable and assign it to an arrow function
+//set parameter
+//filter out all the truthy sheep
+//return the length of the filtered array
+const countSheeps =  arrayOfSheep => arrayOfSheep.filter(sheep=>sheep).length;
