57294c699acf9121176091af512c9b3e2ffbc46c
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-20 21:40:03 -0500

    refactor solution030

--- 8kyu/solution030.js
+++ 8kyu/solution030.js
@@ -10,34 +10,26 @@ Numerical Score	Letter Grade
 60 <= score < 70	'D'
 0 <= score < 60     'F'
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
-P.R.E.P.
-parameters: three numbers 0-100
-return: a string with one letter A-F
-examples:
-input => output
-69, 75, 88 => 'C'
-100, 90, 95 => 'A'
-44, 87, 93 => 'C'
-pseudocode:
-declare function getGrade
-set parameters s1, s2, s3
-assign variable score to (s1 + s2 + s3)/3
-create if else statement to return the correct letter for the score
+
+Will the parameters always be three numbers?
+Will the return always be a string?
+Will the string be "A", "B", "C", "D", or "F"?
+
+console.log(getGrade(95,90,93), 'A);
+console.log(getGrade(70,70,100), 'B');
+console.log(getGrade(70,70,70), 'C');
+console.log(getGrade(65,70,59), 'D');
+console.log(getGrade(44,55,52), 'F');
 */
 
-function getGrade (s1, s2, s3){
-    // Code here
-    let score = (s1 + s2 + s3)/3;
-
-    if(90 <= score && score <= 100){
-        return 'A';
-    }else if(80 <= score && score < 90){
-        return 'B';
-    }else if(70 <= score && score < 80){
-        return 'C';
-    }else if(60 <= score && score < 70){
-        return 'D';
-    }else{
-        return 'F';
-    };
-};
+// declare variable getGrade and assign to arrow function
+// set parameters s1, s2, s3
+const getGrade = (s1, s2, s3) => {
+    // assign variable average to (s1 + s2 + s3)/3
+    let average = (s1 + s2 + s3)/3;
+    // create if else statement to return the correct letter for the score
+    return 90 <= average && average <= 100 ? 'A':
+           80 <= average && average < 90 ? 'B':
+           70 <= average && average < 80 ? 'C':
+           60 <= average && average < 70 ? 'D':'F';
+  };
