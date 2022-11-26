721271e8ed61ef6ee93c20881a4ebde8f91df7f6
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-11-25 20:19:29 -0500

    refactor solution025/8kyu 11/25

--- 8kyu/solution025.js
+++ 8kyu/solution025.js
@@ -3,19 +3,16 @@ Returning Strings
 Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
 [Make sure you type the exact thing I wrote or the program may not execute properly]
-P.R.E.P.
-parameters: name as the string containing a name
-return: "Hello, <name> how are you doing today?".
-examples: 
-input: Roosiel
-output: "Hello, Roosiel how are you doing today?".
-pseudocode:
-create a function
-create a parameter
-return string using template literal
+
+Will the parameter always be a string?
+Will the return always be a string?
+Will the return interpolate the parameter into the string?
+
+console.log(greet("Ryan"), "Hello, Ryan how are you doing today?");
+console.log(greet("Shingles"), "Hello, Shingles how are you doing today?");
 */
 
-function greet(name){
-    //your code here
-    return `Hello, ${name} how are you doing today?`;
-  }
+//declare variable and assign to arrow function
+//set parameter
+//return template literal interpolating parameter into string
+const greet = name => `Hello, ${name} how are you doing today?`;
