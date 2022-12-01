3ae0caae762078d75e0209f5bb2e6a73b87dcd55
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-12-01 18:44:09 -0500

    refactor solution020/8kyu 12/1

--- 8kyu/solution020.js
+++ 8kyu/solution020.js
@@ -1,24 +1,12 @@
 /*
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
-P.R.E.P.
-parameters: boolean value of true or false
-return: a string of "Yes" for true and "No" for false
-examples:
-input: true output: "Yes"
-input: false output: "No"
-pseudocode:
-declare function boolToWord
-create parameter bool
-create if else statement
-use ternary operator
-return appropriate string based on condition
+Will the parameters always be a boolean of true or false?
+Will the return be a string?
+Will the string be "Yes" or "No"?
 */
 
-function boolToWord(bool){
-    return(bool === true ? "Yes" : "No");
-};
-
-function boolToWord(bool){
-    return bool ? "Yes" : "No"; //this works too because it's asking if bool is truthy return "Yes", if not then return "No"
-};
+//declare variable and assign to arrow function
+//set parameter
+//if bool is truthy, return "Yes" else return "No"
+const boolToWord = bool => bool ? "Yes" : "No";
