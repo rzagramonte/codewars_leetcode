b728afb608af4dfa45fd36337d715257af0ebde1
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-12-03 18:20:21 -0500

    refactor solution018/8kyu 12/3

--- 8kyu/solution018.js
+++ 8kyu/solution018.js
@@ -5,17 +5,17 @@ Complete the solution so that it reverses the string passed into it.
 'world'  =>  'dlrow'
 'word'   =>  'drow'
 
-P.R.E.P
-parameters: a string
-return: a string reversed
-examples:
-'world'  =>  'dlrow'
-'word'   =>  'drow'
-pseudocode:
-split the string into substrings
-reverse the order of the strings
-join the reversed order of substrings into one string
+Will the parameter always be a string?
+Will the return always be a string?
+
+console.log(solution('world'), 'dlrow');
+console.log(solution('hello'), 'olleh');
+console.log(solution(''), '');
+console.log(solution('h'), 'h');
 */
-function solution(str){
-    return str.split('').reverse().join('');
-  }
+
+//declare variable and assign to to arrow function
+//split string by character into an array of sub-strings
+//reverse the array in place
+//join the array by character into a string
+const solution = str => str.split('').reverse().join('');
