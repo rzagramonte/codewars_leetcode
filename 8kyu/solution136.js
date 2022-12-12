f0694f860d06ea528e0a17eb4bf4a48cee9ab854
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-12-12 17:02:07 -0500

    codewars solution for 12/12

---
+++ 8kyu/solution136.js
@@ -0,0 +1,33 @@
+/*
+Grasshopper - Array Mean
+
+Find Mean
+Find the mean (average) of a list of numbers in an array.
+
+Information
+To find the mean (average) of a set of numbers add all of the numbers together and divide by the number of values in the list.
+
+For an example list of 1, 3, 5, 7
+
+1. Add all of the numbers
+
+1+3+5+7 = 16
+2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+16/4 = 4
+3. The mean (or average) of this list is 4
+
+Will the parameter always be an array of numbers?
+Will the return always be the average of all the given numbers?
+
+
+*/
+
+console.log(findAverage([1]), 1);
+console.log(findAverage[1,3,5,7], 4);
+
+var findAverage = function (nums) {
+    // Code here
+    // return nums appended to reduce method using dot notation dividied by nums appened to the length property using dot notation
+    return nums.reduce((acc,c)=>acc+c,0) / (nums.length)
+  }
