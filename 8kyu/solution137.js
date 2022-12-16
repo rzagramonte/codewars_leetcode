1dce10692ebcaa9028c5f02373374374c13b4030
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-12-15 19:40:02 -0500

    codewars solution for 12/15

---
+++ 8kyu/solution137.js
@@ -0,0 +1,18 @@
+/*
+Function 2 - squaring an argument
+Now you have to write a function that takes an argument and returns the square of it.
+
+Will the parameter always be an integer?
+Will the return always be an integer as the argument squared?
+
+*/
+
+console.log(square(3),9)
+console.log(square(4),16)
+console.log(square(5),25)
+
+//declare function square and set parameter n
+function square(n){
+    // return n squared using **
+    return n ** 2;
+}
