df91dea82d2c98c9dd9fade30e8ae1b534e47b80
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-12-19 15:37:02 -0500

    update for codewars 12/19

--- 8kyu/solution180.js
+++ 8kyu/solution180.js
@@ -9,10 +9,11 @@ The websites array has already been defined for you using the following code:
 var websites = [];
 
 Will there be no parameters?
-Will there be no return?
+Will the return be an array with "codewars" as its only element?
 
-console.log(websites,"codewars");
+console.log(websites,["codewars"]);
 */
 
 // add the value "codewars" to the already defined websites array
+//declare websites[0] and assign it to "codewars"
 websites[0] = "codewars";
