8e57c92341a9177c63be0c81df0786add1ad0539
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-12-20 15:16:32 -0500

    codewars solution for 12/20

---
+++ 8kyu/solution180.js
@@ -0,0 +1,18 @@
+/*
+Basic Training: Add item to an Array
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+Will there be no parameters?
+Will there be no return?
+
+console.log(websites,"codewars");
+*/
+
+// add the value "codewars" to the already defined websites array
+websites[0] = "codewars";
