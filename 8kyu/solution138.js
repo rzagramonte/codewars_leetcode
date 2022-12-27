bda88a4445333452f2a55fae1d1e273f14c673c9
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2022-12-27 18:57:41 -0500

    codewars solution for 12/27

---
+++ 8kyu/solution138.js
@@ -0,0 +1,33 @@
+/*
+Vowel remover
+
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+don't worry about uppercase vowels
+y is not considered a vowel for this kata
+
+Will the parameters always be one string?
+Will the return always be a string containing only consonants and in all lowercase?
+
+*/
+console.log(shortcut ("hello"),"hll")
+console.log(shortcut ("codewars"),"cdwrs")
+console.log(shortcut ("goodbye"),"gdby")
+console.log(shortcut ("HELLO"),"HELLO")
+
+//declare function and set parameter
+function shortcut (string) {
+    //use return keyword to return the string chained to the replaceAll method using dot notation
+    //pattern should be 'a','e','i','o', and 'u'
+    //replacement should be ''
+  return string.replaceAll('a', '')
+    .replaceAll('e','')
+    .replaceAll('i','')
+    .replaceAll('o','')
+    .replaceAll('u','');
+  }
