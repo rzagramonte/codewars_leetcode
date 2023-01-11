aba257e653585d4e0e41048641681260d8e8ea2a
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-01-11 11:04:35 -0500

    codewars solution for 1/11

---
+++ 8kyu/solution143.js
@@ -0,0 +1,22 @@
+/*
+Reverse List Order
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+Will the parameter always be an array with elements?
+Will the return always be the original array reversed?
+
+console.log(reverseList([1,2,3,4]), [4,3,2,1]);
+console.log(reverseList([3,1,5,4]), [4,5,1,3]);
+*/
+
+//declare function and set parameter
+function reverseList(list) {
+    //use return keyword
+    //append the reverse method using dot notation to the parameter list
+    return list.reverse();
+    }
