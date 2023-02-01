11ae3d18053e7f7caf5eaed0c13853fad273f211
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-02-01 15:20:53 -0500

    codewars solution for 2/1

---
+++ 7kyu/solution006.js
@@ -0,0 +1,29 @@
+/*
+Descending Order
+
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+
+Will the parameter always be a number?
+Will the return always be a number in descending order?
+
+console.log(descendingOrder(0), 0);
+console.log(descendingOrder(1), 1);
+console.log(descendingOrder(111), 111);
+console.log(descendingOrder(15), 51);
+console.log(descendingOrder(1021), 2110);
+console.log(descendingOrder(123456789), 987654321);
+*/
+
+//declare function and set parameter
+function descendingOrder(n){
+    //...
+    //use return keyword, number command, toString, split, sort, and join method to return number in descending order
+    return Number(n.toString().split('').sort((a,b)=>b-a).join(''));
+  }
