3087f018b5eabaa7539aaa7736a58011897e88e6
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-02-11 18:14:40 -0500

    refactor solution017/8kyu 2/11

--- 8kyu/solution017.js
+++ 8kyu/solution017.js
@@ -34,4 +34,4 @@ console.log(flip('L', [1, 4, 5, 3, 5]), [5, 5, 4, 3, 1]);
 //declare variable and assign to arrow function
 //set parameters
 //if d equals "R" then sort the array in ascending order else sort the array in descending order
-const flip = (d, a) => d == 'R' ? a.sort((a,b)=>a-b) : a.sort((a,b)=>b-a);
+const flip = (d, a) =>  a.sort((a,b)=> d == 'R' ? a-b : b-a);
