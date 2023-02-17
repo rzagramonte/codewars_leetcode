89b94094b7acc83198c06d7deb880b7f30626431
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-02-17 17:28:43 -0500

    refactor for solution014/8kyu 2/17

--- 8kyu/solution014.js
+++ 8kyu/solution014.js
@@ -9,22 +9,17 @@ makeNegative(0.12); // return -0.12
 Notes
 The number can be negative already, in which case no change is required.
 Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
-P. - parameters: a number, negative or positive, whole or decimal
-R. - a negative number
-E. - example/edge cases:
-makeNegative(1);    // return -1
-makeNegative(-5);   // return -5
-makeNegative(0);    // return 0
-makeNegative(0.12); // return -0.12
-P. - pseudocode: 
-create function declaration or assign an anon function value to a variable
-create parameter n for the number
-if n < 0 return n
-else return -n
-use the ternary operator!
+
+Will the parameter always be a number?
+Can the number be positive or negative?
+Will the return always be a number?
+Will the number always be negative?
+
+console.log(makeNegative(42), -42);
+console.log(makeNegative(-42), -42);
 */
 
-function makeNegative(num) {
-    // Code?
-    return (num < 0 ? num : -num);
-  }
+//declare variable and assign to arrow function
+//set parameter
+//if num is negative, return num else return -num
+const makeNegative = num => num < 0 ? num : -num;
