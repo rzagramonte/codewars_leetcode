a6ba82187ae7458da3c4bba14a597028c55bbf01
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-02-19 16:58:33 -0500

    refactor solution012/8kyu 2/19

--- 8kyu/solution012.js
+++ 8kyu/solution012.js
@@ -1,26 +1,17 @@
 /*
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-P. - parameters: a number
-R. - return: a string of "Even" or "Odd"
-E. - examples/edge cases: 
-1 - returns "Odd"
-2 - returns "Even"
-3 - returns "Odd"
-4 - returns "Even"
-5 - returns "Odd"
-6 - returns "Even"
-7 - returns "Odd"
-8 - returns "Even"
-9 - returns "Odd"
-10 - returns "Even"
-P. - pseudocode:
-create a function declaration or assign a function value to a const variable
-create a parameter called number
-use ternary operator
-if number is even return "even"
-else return odd
+
+Will the parameter always be a number?
+Will the return always be a string?
+Will the string be either "Even" or "Odd"?
+
+console.log(even_or_odd(2), "Even");
+console.log(even_or_odd(-42), "Even");
+console.log(even_or_odd(-7), "Odd");
+console.log(even_or_odd(0), "Even");
+console.log(even_or_odd(7), "Odd");
 */
 
-function even_or_odd(number) {
-    return (number % 2 === 0 ? "Even" : "Odd");
-}
+//declare variable and assign it to arrow function
+//if number % 2 is truthy, return "Odd" else return "Even"
+const even_or_odd = number => number % 2 ? "Odd" : "Even";
