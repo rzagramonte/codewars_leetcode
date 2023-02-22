2b68c169ae836af05b675f8d182bffb62bd968c9
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-02-22 16:47:24 -0500

    refactor solution011/8kyu 2/22

--- 8kyu/solution011.js
+++ 8kyu/solution011.js
@@ -6,26 +6,16 @@ Example:
 
 None of the arrays will be empty, so you don't have to worry about that!
 
-P. - parameters: an array and its elements; no empty arrays will be passed through
-R. - return: a new array of every other element in the array starting with the first element
-E. - edge cases/examples:
-["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
-P. - pseudocode:
-declare a function
-OR
-set a function value to a variable; create a function expression
-create a parameters - arr
-pass in argument of array with elements
-create for loop that loops starting at index 0 and increments by two
-each iteration should push that element to the new array
-dance
+Will the parameter always be an array?
+Will the return always be an array?
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+console.log(removeEveryOther([[1, 2]]), [[1, 2]]);
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
 */
 
-function removeEveryOther(arr){
-    //your code here
-    let newArr = [];
-    for(let i = 0; i < arr.length; i += 2){
-        return newArr.push(arr[i]);
-    };
-    return newArr; 
-};
+//declare variable and assign to arrow function
+//filter out all elements that do not satisfy callback function
+//callback function: index % 2 === 0
+const removeEveryOther = arr => arr.filter((element,index)=>index % 2 === 0);
