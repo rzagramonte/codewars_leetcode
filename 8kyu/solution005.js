4e907cc125bba13e1e5d914b656a1e9cc19a6bc4
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-03-03 12:57:01 -0500

    refactor for solution005/8kyu 3/3

--- 8kyu/solution005.js
+++ 8kyu/solution005.js
@@ -1,9 +1,16 @@
-// Convert a Boolean to a String
-// Implement a function which convert the given boolean value into its string representation.
-// Note: Only valid inputs will be given.
+/*
+Convert a Boolean to a String
+Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.
 
+Will the parameter always be a boolean?
+Will the return always be a string?
 
-function booleanToString(b){
-    //your code here
-    return b.toString()
-  }
+console.log(booleanToString(true), "true");
+console.log(booleanToString(false), "false");
+*/
+
+//declare const variable and assign to arrow function
+//set parameter
+//convert b to string
+const booleanToString = b => b.toString();
