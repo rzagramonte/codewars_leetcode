4967a26bd07f91c7d98dfa0aab504f2cd9d552d2
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-03-05 12:40:06 -0500

    refactor for solution004/8kyu 3/5

--- 8kyu/solution004.js
+++ 8kyu/solution004.js
@@ -1,20 +1,18 @@
-// Convert a Number to a String!
-// We need a function that can transform a number into a string.
+/*Convert a Number to a String!
+We need a function that can transform a number into a string.
 
-// What ways of achieving this do you know?
+What ways of achieving this do you know?
 
-// Examples:
-// 123 --> "123"
-// 999 --> "999"
-function numberToString(num) {
-    // Return a string of the number here!
-    let str = num.toString();
-    return str
-  }
-// Sample Test
-const assert = require('chai').assert;
-describe("Tests", () => {
-  it("test", () => {
-    assert.strictEqual(numberToString(67), '67');
-  });
-});
+Examples:
+123 --> "123"
+999 --> "999"
+
+Will the parameter always be a number?
+Will the return always be a string?
+
+console.log(numberToString(67), '67');
+*/
+
+//declare const variable and assign to arrow function
+//convert num to string
+const numberToString = num => num.toString();
