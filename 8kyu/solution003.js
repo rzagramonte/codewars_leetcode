d6657dfe933cea67ba55d2a49e4aaabb27c46491
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-03-10 12:32:08 -0500

    refactor for solution003/8kyu 3/10

--- 8kyu/solution003.js
+++ 8kyu/solution003.js
@@ -1,40 +1,26 @@
-// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+/*
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
 
-// P.R.E.P.
-// P: parameters
-// exponent: 0-n; non-negative number
-//
-// R: return is an array of numbers []
-//
-// E: examples
-// 0 = 2^0
-// 1 = 2^1
-// 2 = 2^2
-// 3 = 2^3
-// 4 = 2^4
-// 5 = 2^5
-// 6 = 2^6
-// 7 = 2^7
-//
-//P: pseudo code
-//take in n the positive exponent
-//create a new array to hold all the numbers
-//pass n through a loop
-//use a for loop to pass in the numbers with the given conditional
-//push each result of taking n and placing it at the exponent of base 2 into the end of the array
-//function will return an array of all these numbers
+Will the parameter always be a number?
+Will the number always be a whole number?
+Will the return be an array?
+Will the elements in the array always be numbers?
 
-function powersOfTwo(n){//take in n; the positive exponent
+console.log(powersOfTwo(0), [1]);
+console.log(powersOfTwo(1), [1, 2]);
+console.log(powersOfTwo(4), [1, 2, 4, 8, 16]);
+*/
 
-    let result = [];//create a new array to hold all the numbers - can also be let result = new Array();
-
-    //use a for loop to pass in the numbers with the given conditional
-
-    for (let i = 0; i <= n; i++) {//pass n through a loop
-
-      result.push(Math.pow(2, i));//push each result of taking n and placing it at the exponent of base 2 into the end of the array
-
+//declare const variable and assign it to an arrow function
+//set parameter
+const powersOfTwo = n => {
+  //declare variable named results and assign it to an empty array
+    let results = [];
+    //create for loop
+    for (let i = 0; i <= n; i++) {
+      //push 2 to the i-th power into results
+      results.push(Math.pow(2, i));
     }
-
-    return result;//function will return an array of all these numbers
-  }
+    //return results
+    return results;
+  };
