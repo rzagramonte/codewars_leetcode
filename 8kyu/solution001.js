be3ba16d57f9916e995e1aedc7931d03bd01216b
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-03-13 10:52:05 -0400

    refactor for 8kyu/solution001 3/13

--- 8kyu/solution001.js
+++ 8kyu/solution001.js
@@ -1,30 +1,31 @@
-// Find the smallest integer in the array
-// Given an array of integers your solution should find the smallest integer.
+/*
+Find the smallest integer in the array
+Given an array of integers your solution should find the smallest integer.
 
-// For example:
+For example:
 
-// Given [34, 15, 88, 2] your solution will return 2
-// Given [34, -345, -1, 100] your solution will return -345
-// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
-// P.R.E.P.
-//P: parameters
-//one integer
-//R: return
-//return the smallest integer in an array
-//E: exam/edge cases
-//[5, 6, 2, 6, 1, 9] returns 1
-//[11, 123, 55, 1, -4, 33] returns -4
-//[0, 55, 7, 3, 2, 34] returns 0
-//[23, 978, 3, -57, 90, 1] returns -57
-//P: pseudo code
-//create class
-//create function in class
-//pass in the array
-//return the smallest number in the array
+Will the parameter always be an array?
+Will the elements in the array always be numbers?
+Will the numbers always be integers?
+Will the return always be a number?
+Will the number always be an integer?
 
-class SmallestIntegerFinder {//create class
-    findSmallestInt(args) {//create function in class //pass in the array
-      return Math.min(...args);//return the smallest number in the array
-    }
+let sif = new SmallestIntegerFinder();
+console.log(sif.findSmallestInt([78,56,232,12,8]),8);
+console.log(sif.findSmallestInt([78,56,232,12,18]),12);
+console.log(sif.findSmallestInt([78,56,232,412,228]),56);
+console.log(sif.findSmallestInt([78,56,232,12,0]),0);
+console.log(sif.findSmallestInt([1,56,232,12,8]),1);
+*/
+
+// create class
+class SmallestIntegerFinder {
+  // create arrow function in class
+  // set parameter
+  // return the smallest number in the array
+    findSmallestInt = args => Math.min(...args);
   }
