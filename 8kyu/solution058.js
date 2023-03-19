f11c05efdd9136a1579edc65b2fc8ec8cd946e99
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-03-19 09:32:54 -0400

    update for solution058/8kyu - 3/19

--- 8kyu/solution058.js
+++ 8kyu/solution058.js
@@ -1,20 +1,19 @@
 /*
 Simple multiplication
 This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
-P.R.E.P.
-parameters: number
-return: a product of the given number (number)
-examples:
-input => output 
-2 => 16
-3 => 27
-pseudocode:
-declare function
-set parameter
-if number is even return number times 8 else return number times 9
+
+Will the parameter always be a number?
+Will the return always be a number?
+Will the return's number be a  product of the given number?
+
+console.log(simpleMultiplication(2),16);
+console.log(simpleMultiplication(1),9);
+console.log(simpleMultiplication(8),64);
+console.log(simpleMultiplication(4),32);
+console.log(simpleMultiplication(5),45);
 */
 
-function simpleMultiplication(number) {
-    // your code........
-    return number % 2 === 0 ? number * 8 : number * 9;
-}
+// declare const variable and assign to arrow functin
+// set parameter
+// if number isn't even return number times 9 else return number times 8
+const simpleMultiplication = number => number % 2 ? number * 9 : number * 8;
