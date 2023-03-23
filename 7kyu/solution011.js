656468bbd4557be627da4bcca5c7793ca3ba60c8
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-03-22 20:59:21 -0400

    codewars solution for 3/22

---
+++ 7kyu/solution011.js
@@ -0,0 +1,27 @@
+/*
+Jaden Casing Strings
+
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+Link to Jaden's former Twitter account @officialjaden via archive.org
+
+Will the parameter always be a string?
+Will the return always be a string?
+
+var str = "How can mirrors be real if our eyes aren't real";
+console.log(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't Real");
+*/
+
+//add property to String class/object
+String.prototype.toJadenCase = function () {
+    //...
+    //pattern is first letter of each word
+    //replacement is a function that will change each first letter to its capital letter counterpart
+    return this.replace(/(^|\s)\w/g, firstLetter => firstLetter.toUpperCase());
+  };
