88d6d585bac6d77a0937ef58af49d3247208d74b
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-04-09 00:42:11 -0400

    refactor solution070

--- 8kyu/solution070.js
+++ 8kyu/solution070.js
@@ -3,16 +3,11 @@ Remove exclamation marks
 
 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
-P.R.E.P.
-parameters:
-return:
-examples:
-pseudocode:
-declare function
-set parameter
-use replace method to replace ! with nothing
-return the new string
+Will the parameter always be a string?
+Will the string contain at least one '!'?
+Will the return always be a string?
+Will there be no '!' in the returned string?
+
+console.log(removeExclamationMarks("Hello World!"), "Hello World");
 */
-function removeExclamationMarks(s) {
-    return s.replace(/!/g,'');
-  }
+const removeExclamationMarks = s => s.replaceAll('!', '');
