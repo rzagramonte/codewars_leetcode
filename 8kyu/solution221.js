568623bef65e9639cdf94930323d229f25e9a326
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-04-12 15:30:56 -0400

    codewars solution221

---
+++ 8kyu/solution221.js
@@ -0,0 +1,27 @@
+/*
+My head is at the wrong end!
+
+You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+Simples!
+
+Will the parameter always be an array?
+Will the elements in the array always be strings?
+Will the return always be an array?
+Will the elements in the returned array always be in reverse order compared to the input array?
+
+console.log(fixTheMeerkat(["tail", "body", "head"]), ["head", "body", "tail"]);
+console.log(fixTheMeerkat(["tails", "body", "heads"]), ["heads", "body", "tails"]);
+console.log(fixTheMeerkat(["bottom", "middle", "top"]), ["top", "middle", "bottom"]);
+console.log(fixTheMeerkat(["lower legs", "torso", "upper legs"]), ["upper legs", "torso", "lower legs"]);
+console.log(fixTheMeerkat(["ground", "rainbow", "sky"]), ["sky", "rainbow", "ground"]);
+*/
+
+//declare const variable and assign to arrow function
+//set parameter
+//return the reversed array in place
+const fixTheMeerkat = arr => arr.reverse();
