b738351be1cea1fb1cfcf134b4dfc371a50fe022
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-05-01 15:32:04 -0400

    codewars solution236

---
+++ 8kyu/solution236.js
@@ -0,0 +1,23 @@
+/*
+Unfinished Loop - Bug Fixing #1
+
+Unfinished Loop - Bug Fixing #1
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+Will the parameter always be a number?
+Will the body of the function contain a variable assigned to an empty array, a for loop, and a return value?
+Will the return always be an array?
+Will the elements in the returned array always be numbers?
+
+console.log(createArray(1),[1]);
+console.log(createArray(2),[1,2]);
+console.log(createArray(3),[1,2,3]);
+console.log(createArray(4),[1,2,3,4]);
+console.log(createArray(5),[1,2,3,4,5]);
+*/
+
+//declare const variable and assign to arrow function
+//set parameter
+//declare variable and assign to empty array
+//create for loop with initialization, stopping condition, and final expression
+//return the array
