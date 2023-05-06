06694e9976fc131bcb9be50816af5c6eb22a5fde
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-05-06 12:25:47 -0400

    codewars solution240

---
+++ 8kyu/solution240.js
@@ -0,0 +1,17 @@
+/*
+Basic variable assignment
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+Will the input be three variables, values, and operators?
+Will the output be the same as the input but work correctly?
+
+console.log(name, "codewa.rs");
+*/
+
+//declare three variables
+//assign to values
+//use assignment operator
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
