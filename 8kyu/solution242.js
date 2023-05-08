3e10c1621589da4017bf3a40f3f3f6b237a6ec44
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-05-08 17:59:31 -0400

    codewars solution242

---
+++ 8kyu/solution242.js
@@ -0,0 +1,50 @@
+/*
+Switch/Case - Bug Fixing #6
+
+Switch/Case - Bug Fixing #6
+Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+
+Will the input be a function with a switch case statement in its body?
+Will the output be a function with a switch case statement that returns the correct result value?
+
+console.log(evalObject({a:1,b:1,operation:'+'}), 2);
+console.log(evalObject({a:1,b:1,operation:'-'}), 0);
+console.log(evalObject({a:1,b:1,operation:'/'}), 1);
+console.log(evalObject({a:1,b:1,operation:'*'}), 1);
+console.log(evalObject({a:1,b:1,operation:'%'}), 0);
+console.log(evalObject({a:1,b:1,operation:'^'}), 1);
+*/
+
+
+//declare const variable and assign to arrow function
+//set parameter
+const evalObject = value => {
+    //declare variable
+    var result;
+    //create switch case statement
+    //variable for switch will be the operation property in the object passed in
+    //assign the appropriate expressions for each case
+    //break out of each case
+    switch(value.operation){
+      case'+':
+        result = value.a + value.b;
+        break;
+      case'-':
+        result = value.a - value.b;
+        break;
+      case'/':
+        result = value.a / value.b;
+        break;
+      case'*':
+        result = value.a * value.b;
+        break;
+      case'%':
+        result = value.a % value.b;
+        break;
+      case'^':
+        result = Math.pow(value.a, value.b);
+        break;
+    }
+    //return result
+    return result;
+  };
