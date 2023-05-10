f6ca28bd9557e50235affdd9367d03fd20fc69e3
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-05-10 13:49:32 -0400

    codewars solution253

---
+++ 8kyu/solution253.js
@@ -0,0 +1,19 @@
+/*
+Grasshopper - Variable Assignment Debug
+
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+Will the input be three variables, operators, and values?
+Will the output have correct operators to enable assignment?
+
+console.log(a, 'dev);
+console.log(b, 'Lab');
+console.log(name, 'devLab');
+*/
+
+//change loose equality operator to assignment operator
+var a = "dev";
+var b = "Lab";
+
+var name = a + b;
