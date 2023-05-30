66abf4917c793a9b540e9970fb1f5b593d69af9d
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-05-30 10:40:55 -0400

    codewars solution273

---
+++ 8kyu/solution273.js
@@ -0,0 +1,19 @@
+/*
+Return to Sanity
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+//original code
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return
+    results;
+}
+*/
+
+//fixed code
+function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return results;
+  }
