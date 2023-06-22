da637a829a31329504bde0a950340e2d12ac9a9d
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-06-21 20:03:07 -0400

    codewars solution035

---
+++ 7kyu/solution035.js
@@ -0,0 +1,30 @@
+/*
+Mumbling
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+Will the parameter always be a string?
+Will the string only contain upper or lowercase letters?
+Will the return always be a string?
+Will the string contain one specific pattern of letters, upper or lowercase, separated by dashes?
+
+console.log(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+console.log(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+console.log(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+console.log(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+console.log(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+*/
+
+//declare a constant variable that will be assigned to an arrow function
+//convert s to all lowercase
+//split s by character and pass in each character as an element in a new array
+//iterate over the array
+//each letter will be reassigned to letter in uppercase concatenated by l repeated i many times where i is the index of the element
+//join the new array passing in a dash as the separator
+const accum=s=>s.toLowerCase().split('').map((l,i)=>l=`${l.toUpperCase()}${l.repeat(i)}`).join('-');
