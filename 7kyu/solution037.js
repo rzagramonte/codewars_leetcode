67e124ba52dd2610ff2531c23df8df92027c8c3b
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-06-25 13:50:21 -0400

    codewars solution037

---
+++ 7kyu/solution037.js
@@ -0,0 +1,35 @@
+/*
+Simple remove duplicates
+
+Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+
+More examples can be found in the test cases.
+
+Good luck!
+
+Will the parameter always be an array?
+Will the return always be an array?
+
+console.log(solve([3,4,4,3,6,3]),[4,6,3]);
+console.log(solve([1,2,1,2,1,2,3]),[1,2,3]);
+console.log(solve([1,2,3,4]),[1,2,3,4]);
+console.log(solve([1,1,4,5,1,2,1]),[4,5,2,1]);
+console.log(solve([1,2,1,2,1,1,3]),[2,1,3]);
+*/
+
+//declare a constant variable which will be assigned to an arrow function
+//set parameter
+//create new array using square brackets
+//create new set using constructor
+//spread the values of the Set object into the new array
+//pass in the reversed input array into the set constructor as an argument
+//reverse the new array created
+const solve = arr => [...new Set(arr.reverse())].reverse();
