e85ed2eb71d7d1a2836ebc9b9393741502ab18a3
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-11-28 12:36:35 -0500

    Flatten

---
+++ 7kyu/solution150.js
@@ -0,0 +1,24 @@
+/*
+Flatten
+
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+
+Will the parameter always be an array?
+Will the return always be an array?
+
+console.log(flatten([]), []);
+console.log(flatten([1, 2, 3]), [1, 2, 3]);
+console.log(flatten([[1, 2, 3], ["a", "b", "c"], [1, 2, 3]]), [1, 2, 3, "a", "b", "c", 1, 2, 3]);
+console.log(flatten([[3, 4, 5], [[9, 9, 9]], ["a,b,c"]]), [3, 4, 5, [9, 9, 9], "a,b,c"]);
+console.log(flatten([[[3], [4], [5]], [9], [9], [8], [[1, 2, 3]]]), [[3], [4], [5], 9, 9, 8, [1, 2, 3]]);
+*/
+
+//declare constant variable flatten which will be assigned to an arrow function
+//set parameter array
+//flatten array only one level
+const flatten = array => array.flat(1);
