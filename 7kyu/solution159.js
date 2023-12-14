65d67c7ff184ab97b3d37940b6a516da69bd6929
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-12-13 21:11:00 -0500

    Tidy Number (Special Numbers Series #9)

---
+++ 7kyu/solution159.js
@@ -0,0 +1,52 @@
+/*
+Tidy Number (Special Numbers Series #9)
+
+Definition
+A Tidy number is a number whose digits are in non-decreasing order.
+
+Task
+Given a number, Find if it is Tidy or not .
+
+Notes
+Number passed is always Positive .
+
+Return the result as a Boolean
+
+Input >> Output Examples
+tidyNumber (12) ==> return (true)
+Explanation:
+The number's digits { 1 , 2 } are in non-Decreasing Order (i.e) 1 <= 2 .
+tidyNumber (32) ==> return (false)
+Explanation:
+The Number's Digits { 3, 2} are not in non-Decreasing Order (i.e) 3 > 2 .
+
+tidyNumber (1024) ==> return (false)
+Explanation:
+The Number's Digits {1 , 0, 2, 4} are not in non-Decreasing Order as 0 <= 1 .
+
+tidyNumber (13579) ==> return (true)
+Explanation:
+The number's digits {1 , 3, 5, 7, 9} are in non-Decreasing Order .
+
+tidyNumber (2335) ==> return (true)
+Explanation:
+The number's digits {2 , 3, 3, 5} are in non-Decreasing Order , Note 3 <= 3
+
+Will the parameter always be a number?
+Will the return always be a boolean of either true or false?
+
+console.log(tidyNumber(12),true);
+console.log(tidyNumber(102),false);
+console.log(tidyNumber(9672),false);
+console.log(tidyNumber(2789),true);
+console.log(tidyNumber(2335),true);
+*/
+
+//declare function and set parameter
+//create new array spreading the digits as a string
+//sort them as strings
+//join them
+//does it equal in value to n?
+function tidyNumber(n){
+    return [...n + ''].sort().join('') == n;
+  }
