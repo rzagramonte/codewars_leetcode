3513248d31e9b3433c6ebd7b2c5c5899a5973cc8
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2023-12-31 18:57:21 -0500

    For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ 8kyu/solution340.py
@@ -0,0 +1,30 @@
+'''
+For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+
+This is a beginner friendly kata especially for UFC/MMA fans.
+
+It's a fight between the two legends: Conor McGregor vs George Saint Pierre in Madison Square Garden. Only one fighter will remain standing, and after the fight in an interview with Joe Rogan the winner will make his legendary statement. It's your job to return the right statement depending on the winner!
+
+If the winner is George Saint Pierre he will obviously say:
+
+"I am not impressed by your performance."
+If the winner is Conor McGregor he will most undoubtedly say:
+
+"I'd like to take this chance to apologize.. To absolutely NOBODY!"
+Good Luck!
+
+Note
+The given name may varies in casing, eg., it can be "George Saint Pierre" or "geOrGe saiNT pieRRE". Your solution should treat both as the same thing (case-insensitive).
+
+Will the parameter always be a string?
+Will the return always be a string?
+
+print(quote('George Saint Pierre'), "I am not impressed by your performance.")
+print(quote('Conor McGregor'), "I'd like to take this chance to apologize.. To absolutely NOBODY!")
+print(quote('george saint pierre'), "I am not impressed by your performance.")
+print(quote('conor mcgregor'), "I'd like to take this chance to apologize.. To absolutely NOBODY!")
+'''
+
+#use ternary conditional
+def quote(fighter):
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!" if fighter.lower() == 'conor mcgregor' else "I am not impressed by your performance."
