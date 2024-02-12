9394a840297ed720adc1d00dc05ed9155ae46f5c
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-02-12 04:54:05 -0500

    Conditional statement--switch - refactored

--- 8kyu/solution114.js
+++ 8kyu/solution114.js
@@ -62,3 +62,20 @@ function howManydays(month){
           break;
     }
   }
+
+//refactored code:
+const howManydays = month => {
+  switch (month){
+    case 1: return 31;
+    case 2: return 28;
+    case 3: return 31;
+    case 4: return 30;
+    case 5: return 31;
+    case 6: return 30;
+    case 7: return 31;
+    case 8: return 31;
+    case 9: return 30;
+    case 10: return 31;
+    case 11: return 30;
+    case 12: return 31;
+  };
