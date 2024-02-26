d40db95ff303660a2796de4bb02e16ff1a7bce6e
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-02-25 23:32:34 -0500

    are arrow functions odd - refactored

--- 8kyu/solution218.js
+++ 8kyu/solution218.js
@@ -18,4 +18,22 @@ console.log(odds([1, 2, 3, 4, 5, 6]), [1, 3, 5]);
 //declare const variable and assign to an arrow function
 //append values to the filter method
 //callback function should test if each element is not even
-const odds = values => values.filter( element => element % 2 !== 0);
+//const odds = values => values.filter( element => element % 2 !== 0);
+
+//refactored code:
+/*
+Will the parameter always be an array?
+Will the elements of the input array always be numbers, if any?
+Will the return always be an array?
+Will the elements of the output array always be numbers, if any?
+
+console.log(odds([]), []);
+console.log(odds([2, 4, 6]), []);
+console.log(odds([1, 3, 5]), [1, 3, 5]);
+console.log(odds([1, 2, 3, 4, 5, 6]), [1, 3, 5]);
+*/
+
+//declare const variable and assign to an arrow function
+//append values to the filter method
+//callback function should test if each element is not even
+const odds = values => values.filter(n => n%2);
