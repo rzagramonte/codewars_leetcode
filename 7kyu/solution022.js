b1ee43439030b23f424fc9c05fce37730cd2288d
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-02-29 02:25:00 -0500

    No oddities here - refactored code

--- 7kyu/solution022.js
+++ 7kyu/solution022.js
@@ -17,4 +17,7 @@ console.log(noOdds([0,1,2,3]), [0,2]);
 //declare const variable and assign to arrow function
 //set parameter
 //filter out elements that are even
-const noOdds = values => values.filter(value => value % 2 == 0);
+//const noOdds = values => values.filter(value => value % 2 == 0);
+
+//refactored code:
+const noOdds = values => values.filter(value => !(value % 2));
