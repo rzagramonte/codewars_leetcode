f199d0f373e3d97dc3503b76b60f543de39ad3ed
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-03-14 01:32:57 -0400

    Alternate case - refactored

--- 7kyu/solution025.js
+++ 7kyu/solution025.js
@@ -18,4 +18,11 @@ console.log(alternateCase("Hello World"), "hELLO wORLD");
 //is letter equal in value to the letter as uppercase? then return it as a lowercase
 //else return it as an uppercase
 //join the elements in the array by character
-const alternateCase = s => s.split('').map(letter=>letter == letter.toUpperCase() ? letter.toLowerCase():letter.toUpperCase()).join('');
+// const alternateCase = s => s.split('').map(letter=>letter == letter.toUpperCase() ? letter.toLowerCase():letter.toUpperCase()).join('');
+
+//refactored code:
+//create an array from input string
+//map over the items in the array
+//if item is equal in value to uppercase item, then return lowercase item, else return uppercase item
+//convert array back to string
+const alternateCase = s => Array.from(s, c => c == c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()).join('');
