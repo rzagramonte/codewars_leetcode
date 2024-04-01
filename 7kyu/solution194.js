bf2413b5935044e5e54beae5594e0ec40e708fe0
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-04-01 15:50:41 -0400

    Love vs friendship

---
+++ 7kyu/solution194.js
@@ -0,0 +1,33 @@
+/*
+Love vs friendship
+
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice as strong as love :-)
+
+Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+The input will always be made of only lowercase letters and will never be empty.
+*/
+
+/*
+Will the argument passed into the function's parameter always be a string?
+Will the return always be a number?
+
+console.log(wordsToMarks("attitude"), 100);
+console.log(wordsToMarks("friends"), 75);
+console.log(wordsToMarks("family"), 66);
+console.log(wordsToMarks("selfness"), 99);
+console.log(wordsToMarks("knowledge"), 96);
+*/
+
+//create object of letter:number
+//create array with string, map over it flipping each letter to its corresponding number, and reduce to one value
+const wordsToMarks = string => {
+    const alphabet = {a:1,b:2,c:3,d:4,e:5,f:6,g:7,h:8,i:9,j:10,k:11,l:12,m:13,n:14,o:15,p:16,q:17,r:18,s:19,t:20,u:21,v:22,w:23,x:24,y:25,z:26};
+    return [...string].map(l=>alphabet[l]).reduce((a,c)=>a+c,0);
+};
