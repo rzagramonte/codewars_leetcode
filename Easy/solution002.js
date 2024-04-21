533912f739294a6296c6be893c9bb3b1c1b2c6df
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-04-20 21:44:50 -0400

    2620. Counter - leetcode

---
+++ Easy/solution002.js
@@ -0,0 +1,59 @@
+/*
+2620. Counter
+
+Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+ 
+
+Example 1:
+
+Input: 
+n = 10 
+["call","call","call"]
+Output: [10,11,12]
+Explanation: 
+counter() = 10 // The first time counter() is called, it returns n.
+counter() = 11 // Returns 1 more than the previous time.
+counter() = 12 // Returns 1 more than the previous time.
+Example 2:
+
+Input: 
+n = -2
+["call","call","call","call","call"]
+Output: [-2,-1,0,1,2]
+Explanation: counter() initially returns -2. Then increases after each sebsequent call.
+ 
+
+Constraints:
+
+-1000 <= n <= 1000
+0 <= calls.length <= 1000
+calls[i] === "call"
+
+
+Will the argument passed into the function's parameter always be an integer?
+Will the return always be an integer?
+*/
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+//decrement n by 1
+//in closure: increment n
+//return n
+var createCounter = function(n) {
+    n-=1;
+    return function() {
+        n++
+        return n;
+    };
+    
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
