09ed60598aabf77aee7d01ecebd7bf99e9dae6b1
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-05-25 20:34:21 -0400

    Basic Sequence Practice

---
+++ 7kyu/solution204.js
@@ -0,0 +1,41 @@
+/*
+Basic Sequence Practice
+
+A sequence or a series, in mathematics, is a string of objects, like numbers, that follow a particular pattern. The individual elements in a sequence are called terms. A simple example is 3, 6, 9, 12, 15, 18, 21, ..., where the pattern is: "add 3 to the previous term".
+
+In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+
+[ 0,  1,    3,      6,   ...]
+  0  0+1  0+1+2  0+1+2+3
+Your Task
+Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic series explained above. Whenn < 0 return the sequence with negative terms.
+
+Examples
+5  -->  [0,  1,  3,  6,  10,  15]
+-5  -->  [0, -1, -3, -6, -10, -15]
+7  -->  [0,  1,  3,  6,  10,  15,  21,  28]
+
+Will the argument passed into the function's parameter always be a number?
+Will the number always be an integer?
+Will the return be an array of integers?
+
+console.log(sumOfN(3), [0, 1, 3, 6]);
+console.log(sumOfN(-4), [0, -1, -3, -6, -10]);
+console.log(sumOfN(1), [0, 1]);
+console.log(sumOfN(-6), [0, -1, -3, -6, -10, -15, -21]);
+
+create variables for cumulative, sign, and array
+create for loop where each iteration pushes index + cumulative * sign to the array
+add index to cumulative and reassign cumulative to sum
+return array
+*/
+const sumOfN = n => {
+    let c = 0;
+    const s = Math.sign(n);
+    const a = [];
+    for(let i = 0; i <= Math.abs(n); i++){
+      a.push((i + c) * s);
+      c += i;
+    };
+    return a;
+};
