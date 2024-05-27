01d89c878db20e1a081b0b13b81e01d529c01e44
rzagramonte <100784269+rzagramonte@users.noreply.github.com>
2024-05-26 23:45:40 -0400

    Simple beads count

---
+++ 7kyu/solution208.js
@@ -0,0 +1,22 @@
+/*
+Simple beads count
+
+Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.
+
+Will the argument passed into the function's parameter always be a number?
+Will the return always be a number?
+
+console.log(countRedBeads(0), 0);
+console.log(countRedBeads(1), 0);
+console.log(countRedBeads(3), 4);
+console.log(countRedBeads(5), 8);
+
+if n is less than 2 return 0 else return n-1 time 2
+*/
+
+const countRedBeads = n => n<2?0:(n-1)*2;
