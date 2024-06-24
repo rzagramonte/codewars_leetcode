c97eea61f1534600844dcd59ffbab44a9279a367
rzagramonte <rzagramonte@outlook.com>
2024-06-23 23:03:44 -0400

    Polish alphabet

---
+++ 8kyu/solution396.js
@@ -0,0 +1,39 @@
+/*
+Polish alphabet
+
+There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+Your task is to change the letters with diacritics:
+
+ą -> a,
+ć -> c,
+ę -> e,
+ł -> l,
+ń -> n,
+ó -> o,
+ś -> s,
+ź -> z,
+ż -> z
+and print out the string without the use of the Polish letters.
+
+For example:
+
+"Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+Will the argument passed into the function's parameter always be a string?
+Will the return always be a string?
+
+console.log(correctPolishLetters("Jędrzej Błądziński"),"Jedrzej Bladzinski");
+console.log(correctPolishLetters("Lech Wałęsa"),"Lech Walesa");
+console.log(correctPolishLetters("Maria Skłodowska-Curie"),"Maria Sklodowska-Curie");
+
+TODO:
+create object of polish alphabet
+iterate over string
+if the letter is found as a key in the object, return the value else return the letter
+*/
+
+const correctPolishLetters = str => {
+    const polishAlphabet = { ą: "a", ć: "c", ę: "e", ł: "l", ń: "n", ó: "o", ś: "s", ź: "z", ż: "z"};
+    return [...str].map(e=>polishAlphabet[e] || e).join('');
+  };
