509af8aea971009d8ff35d236179314d58242f5e
rzagramonte <rzagramonte@outlook.com>
2024-07-28 19:19:05 -0400

    Area or Perimeter

--- 8kyu/solution039.js
+++ 8kyu/solution039.js
@@ -1,4 +1,6 @@
 /*
+Area or Perimeter
+
 You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
 If it is a square, return its area. If it is a rectangle, return its perimeter.
 
@@ -8,6 +10,7 @@ Example(Input1, Input2 --> Output):
 3, 3 --> 9
 Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
 
+old:
 P.R.E.P.
 parameters: two numbers l for length and w for width
 return: if l === w return area if l > w || l < w return perimeter
@@ -20,6 +23,17 @@ declare function
 set parameters
 create if else statement using ternary operator
 if l === w return area else return perimeter
+const areaOrPerimeter = (l , w) => l === w ? l * w : 2 * l + 2 * w;
+
+new:
+Will the argument passed into the function always be positive integers?
+Will the return always be a number?
+
+console.log(areaOrPerimeter(4 , 4) , 16);
+console.log(areaOrPerimeter(6 , 10) , 32);
+
+TODO:
+find area or perimeter depending on input
 */
 
-const areaOrPerimeter = (l , w) => l === w ? l * w : 2 * l + 2 * w;
+const areaOrPerimeter = (l , w) => l == w ? l * w : 2 * (l + w);
