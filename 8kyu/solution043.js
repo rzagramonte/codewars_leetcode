bed2e68804c9f078440055715020cc75a54b50b6
rzagramonte <rzagramonte@outlook.com>
2024-07-29 19:39:25 -0400

    Training JS #2: Basic data types--Number

--- 8kyu/solution043.js
+++ 8kyu/solution043.js
@@ -9,6 +9,7 @@ Numbers can use operators such as + - * / %
 Task
 I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
 
+old:
 P.R.E.P.
 parameters: two numbers from v1 to v6
 return: a number value of 100
@@ -21,8 +22,6 @@ input => output
 (250, 150) => 100
 pseudocode:
 insert the different variables into the corresponding functions
-*/
-
 let v1 = 50,
     v2 = 100,
     v3 = 150,
@@ -60,3 +59,24 @@ function equal5(){
       b =  v3; //set number value to b
   return a % b;
 }
+
+new:
+Will the arguments passed into the function always be two numbers?
+Will the return always be a number?
+
+console.log(equal1(),100);
+console.log(equal2(),100);
+console.log(equal3(),100);
+console.log(equal4(),100);
+console.log(equal5(),100);
+
+TODO:
+set a and b as parameters w default values of v1-v6
+*/
+
+let v1 = 50, v2 = 100, v3 = 150, v4 = 200, v5 = 2, v6 = 250;
+const equal1 = (a = v1, b = v1) => a + b;
+const equal2 = (a =  v3, b =  v1) => a - b;
+const equal3 = (a =  v1, b =  v5) => a * b;
+const equal4 = (a =  v4, b =  v5) => a / b;
+const equal5 = (a =  v6, b =  v3) => a % b;
