e1f871fc36be0a7cf5730becb8902b7eaa5a3cf3
rzagramonte <rzagramonte@outlook.com>
2024-07-30 19:54:27 -0400

    Find Maximum and Minimum Values of a List

--- 8kyu/solution061.js
+++ 8kyu/solution061.js
@@ -12,6 +12,7 @@ Examples (Input -> Output)
 Notes
 You may consider that there will not be any empty arrays/vectors.
 
+old:
 P.R.E.P.
 paramters: an array
 return: a number representing min or max of the array
@@ -27,7 +28,6 @@ set parameters
 for min return Math.min(...list) - must use spreader to deconstruct the array
 for max return Math.max(...list) - must use spreader to deconstruct the array
 use return keyword for both
-*/
 
 var min = function(list){
     
@@ -38,3 +38,18 @@ var max = function(list){
     
     return Math.max(...list);
 }
+
+new:
+Will the arguments passed into the functions always be an array of numbers?
+Will the return always be a number?
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]), -110);
+console.log(min([42, 54, 65, 87, 0]), 0);
+console.log(max([4,6,2,1,9,63,-134,566]), 566);
+console.log(max([5]), 5);
+
+TODO:
+return the min and max number for each function, respectively
+*/
+
+let min = list => Math.min(...list), max = list => Math.max(...list);
