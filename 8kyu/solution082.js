34e048c1a150e392ae51105e442d235f9c34987b
rzagramonte <rzagramonte@outlook.com>
2024-08-02 20:32:01 -0400

    Reversed Words

--- 8kyu/solution082.js
+++ 8kyu/solution082.js
@@ -6,6 +6,7 @@ Example(Input --> Output):
 
 "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
+old:
 P.R.E.P.
 parameters: a string
 return: a reversed string
@@ -20,8 +21,26 @@ return str appended to the method split with its argument as ' ' appended to the
 this will split the string into an array of the words
 then reverse the order of the array
 then join the words back into a string seperated by a space
-*/
 
 function reverseWords(str){
     return str.split(' ').reverse().join(' '); // reverse those words
   }
+
+new:
+Will the argument passed into the function always be a string?
+Will the return always be a string?
+
+
+console.log(reverseWords("hello world!"), "world! hello")
+console.log(reverseWords("yoda doesn't speak like this" ),  "this like speak doesn't yoda")
+console.log(reverseWords("foobar"                       ),  "foobar")
+console.log(reverseWords("kata editor"                  ),  "editor kata")
+console.log(reverseWords("row row row your boat"        ),  "boat your row row row")
+
+TODO:
+split string by words
+reverse
+join string by words
+*/
+
+const reverseWords = s => s.split(' ').reverse().join(' ');
