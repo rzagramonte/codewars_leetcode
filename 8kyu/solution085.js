8ab6cc9eadef7010a064ed311751c829224f899b
rzagramonte <rzagramonte@outlook.com>
2024-08-04 13:44:56 -0400

    Name Shuffler

--- 8kyu/solution085.js
+++ 8kyu/solution085.js
@@ -6,6 +6,8 @@ Example(Input --> Output)
 
 "john McClane" --> "McClane john"
 
+old:
+
 P.R.E.P.
 parameter: a string
 return: a string reversed
@@ -18,8 +20,24 @@ declare function
 set parameter
 return str appended to split() with its argument as '
 ' appended to reverse() appended to join() with its argument as ' '
-*/
+
 function nameShuffler(str){
     //Shuffle It
     return str.split(' ').reverse().join(' ');
   }
+
+new:
+
+Will the argument passed into the function always be a string?
+Will the return always be a string?
+
+console.log(nameShuffler('john McClane'),'McClane john');
+console.log(nameShuffler('Mary jeggins'),'jeggins Mary')
+console.log(nameShuffler('tom jerry'),'jerry tom');
+
+split string into words
+reverse
+join array to string of words
+*/
+
+const nameShuffler = str => str.split(' ').reverse().join(' ');
