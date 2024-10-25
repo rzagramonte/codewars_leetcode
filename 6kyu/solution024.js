ff4c55544469eeaf84e07a95d0831c565043f9ce
Roosiel <rzagramonte@outlook.com>
2024-10-25 01:31:06 -0400

    Sort the odd

---
+++ 6kyu/solution024.js
@@ -0,0 +1,33 @@
+/*
+Sort the odd
+
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+*/
+
+/*
+arr of nums, arr can be empty, nums will be whole, nums won't be sorted in any way
+arr of nums where odd nums will be acsending and even numbers will be in the same spot
+
+TODO:
+sort the odd values
+even values remain in the same position
+filtering, sorting, inserting back into the array
+
+*/
+
+const sortArray = array => {
+    const oddArr = array.filter(n=>n%2).sort((a,b)=>a-b);
+    return array.map(n=>n%2 ? oddArr.shift() : n);
+};
+
+console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+console.log(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+console.log(sortArray([]),[]);
+  
+  
