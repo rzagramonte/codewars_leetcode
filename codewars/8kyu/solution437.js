a6c537e2caf309159a395d281e7b77650949e775
Roosiel <rzagramonte@outlook.com>
2025-05-17 20:40:58 -0400

    Not so black box

---
+++ codewars/8kyu/solution437.js
@@ -0,0 +1,13 @@
+/*
+Not so black box
+
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+describe("Did you open the box?", function() {
+ 
+    Test.expect(check(), "It's clamped shut!");
+   
+});
+*/
+
+JSopenSesame();
