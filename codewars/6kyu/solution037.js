5e31e38e1b0e1df36f1fb8319c97addfa7a859c3
Roosiel <rzagramonte@outlook.com>
2025-08-27 14:00:00 -0400

    Your order, please

---
+++ codewars/6kyu/solution037.js
@@ -0,0 +1,29 @@
+/*
+Your order, please
+
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+
+Will the argument passed into the function always be a string?
+Will the return always be a string?
+
+console.log(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+console.log(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+console.log(order(""), "", "empty input should return empty string" )
+
+TODO:
+split string by whitespace
+sort by digit matched in the string
+will be first element in the returned array
+join string by whitespace
+*/
+
+const order = words => words.split(' ').sort((a,b) => a.match(/\d/g)[0]-b.match(/\d/g)[0]).join(' ');
